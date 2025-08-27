5ce66b108604836451b96c262a765c6ba0a3a213
jenni <jenni@jenniwhitman.com>
2025-08-27 12:06:52 -0400

    update base url

--- sympathy-site/vite.config.js
+++ sympathy-site/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/jak-card/sympathy-site/',
   plugins: [react(), tailwindcss()],
 })
